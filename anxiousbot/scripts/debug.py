0153269bc6a1088279eaec34387ef0c6c604a863
Filipe C Menezes <fcmenezes87@gmail.com>
2024-06-28 16:13:45 +0100

    feat: update debugger to handle multi coin

--- anxiousbot/scripts/debug.py
+++ anxiousbot/scripts/debug.py
@@ -1,5 +1,7 @@
 import asyncio
+import itertools
 import os
+from datetime import datetime
 
 import ccxt.pro as ccxt
 import tabulate
@@ -10,20 +12,22 @@ def _match_book_orders(book_orders):
     deals = []
     for a in book_orders:
         for b in book_orders:
-            if b['name'] == a['name']:
+            if b["name"] == a["name"]:
                 continue
             deal = _match_asks_bids(a["asks"], b["bids"])
+            deal["ts"] = str(datetime.now())
             deal["symbol"] = a["symbol"]
             deal["buy"]["name"] = a["name"]
             deal["sell"]["name"] = b["name"]
             deals += [deal]
 
             deal = _match_asks_bids(b["asks"], a["bids"])
+            deal["ts"] = str(datetime.now())
             deal["symbol"] = a["symbol"]
             deal["buy"]["name"] = b["name"]
             deal["sell"]["name"] = a["name"]
             deals += [deal]
-    return [deal for deal in deals if deal['potential_profit'] > 0]
+    return [deal for deal in deals if deal["potential_profit"] > 3]
 
 
 def _match_asks_bids(buy_asks, sell_bids):
@@ -40,6 +44,9 @@ def _match_asks_bids(buy_asks, sell_bids):
     buy_orders = []
     sell_orders = []
 
+    buy_total = 0
+    sell_total = 0
+
     while buy_index < len(buy_asks) and sell_index < len(sell_bids):
         buy_price = buy_asks[buy_index][0]
         buy_amount = buy_asks[buy_index][1]
@@ -55,6 +62,8 @@ def _match_asks_bids(buy_asks, sell_bids):
             sell_price_max = max(sell_price_max, sell_price)
 
             matched_amount = min(buy_amount, sell_amount)
+            buy_total += matched_amount * buy_price
+            sell_total += matched_amount * sell_price
             total_amount += matched_amount
             potential_profit += matched_amount * (sell_price - buy_price)
 
@@ -80,16 +89,18 @@ def _match_asks_bids(buy_asks, sell_bids):
         "buy": {
             "orders": buy_orders,
             "price": {"min": buy_price_min, "max": buy_price_max},
+            "total": buy_total,
         },
         "sell": {
             "orders": sell_orders,
             "price": {"min": sell_price_min, "max": sell_price_max},
+            "total": sell_total,
         },
     }
 
 
 def _print_book_orders(book_orders):
-    no_lines = 20
+    no_lines = 5
 
     red_color = "\033[91m"
     green_color = "\033[92m"
@@ -124,46 +135,113 @@ def _print_book_orders(book_orders):
 
         tabs += [table_combined.split("\n")]
 
-    os.system("cls" if os.name == "nt" else "clear")
-
-    print(book_orders[0]["symbol"])
     print()
+    print(book_orders[0]["symbol"])
     lines = "\n".join(["    ".join(columns) for columns in zip(*tabs)])
     print(lines)
 
 
 def _print_deals(deals):
-    print(tabulate.tabulate(
-        [[deal['total_amount'], deal['potential_profit'], deal['buy']
-            ['name'], deal['sell']['name']] for deal in deals],
-            headers=("Total Amount", "Potential Profit", "Buy Exchange", "Sell Exchange")
-    ))
+    print("Deals")
+    print(
+        tabulate.tabulate(
+            [
+                [
+                    deal["ts"],
+                    deal["symbol"],
+                    deal["buy"]["total"],
+                    deal["sell"]["total"],
+                    deal["potential_profit"],
+                    deal["buy"]["name"],
+                    deal["sell"]["name"],
+                ]
+                for deal in deals
+            ],
+            headers=(
+                "Timestamp",
+                "Symbol",
+                "Total Buy",
+                "Total Sell",
+                "Potential Profit",
+                "Buy Exchange",
+                "Sell Exchange",
+            ),
+            floatfmt=".8f",
+        )
+    )
+
+
+def _common_symbol_to_exchange(symbol, exchange_id):
+    if symbol == "GALA/USDT" and exchange_id == "kucoin":
+        return "GALAX/USDT"
+    return symbol
+
+
+def _exchange_symbol_to_common(symbol, exchange_id):
+    if symbol == "GALAX/USDT" and exchange_id == "kucoin":
+        return "GALA/USDT"
+    return symbol
+
 
 async def _run():
-    clients = [ccxt.binance(), ccxt.kucoin(), ccxt.bitget()]
+    binance_client = ccxt.binance()
+    bitget_client = ccxt.bitget()
+    kucoin_client = ccxt.kucoin()
+    gateio_client = ccxt.gateio()
+    poloniex_client = ccxt.poloniex()
+    clients = [
+        binance_client,
+        bitget_client,
+        kucoin_client,
+        gateio_client,
+        poloniex_client,
+    ]
 
-    symbol = "BTC/USDT"
+    symbols = ["GALA/USDT", "BTC/USDT", "ETH/USDT", "XRP/USDT", "LTC/USDT"]
     try:
+        deals = []
         while True:
-            book_orders = []
-            for client in clients:
-                book_order = await client.watch_order_book(symbol)
-                book_orders += [
-                    {
-                        "name": client.name,
-                        "symbol": symbol,
-                        "asks": book_order["asks"],
-                        "bids": book_order["bids"],
-                    }
-                ]
+            book_orders_per_symbol = []
+            tasks = []
+            for symbol in symbols:
+                for client in clients:
 
-            _print_book_orders(book_orders)
+                    async def _fetch_book_order(client, symbol):
+                        book_order = await client.watch_order_book(symbol)
+                        return {
+                            "id": client.id,
+                            "name": client.name,
+                            "symbol": symbol,
+                            "asks": book_order["asks"],
+                            "bids": book_order["bids"],
+                        }
+
+                    tasks += [
+                        asyncio.create_task(
+                            _fetch_book_order(
+                                client, _common_symbol_to_exchange(symbol, client.id)
+                            )
+                        )
+                    ]
+            book_orders = await asyncio.gather(*tasks)
+            book_orders_per_symbol = []
+            for symbol, orders in itertools.groupby(
+                book_orders,
+                key=lambda x: _exchange_symbol_to_common(x["symbol"], x["id"]),
+            ):
+                book_orders_per_symbol += [list(orders)]
+
+            os.system("cls" if os.name == "nt" else "clear")
+            for book_orders in book_orders_per_symbol:
+                _print_book_orders(book_orders)
+                deals += _match_book_orders(book_orders)
+            while len(deals) > 30:
+                deals = deals[1:]
 
             print()
-            deals = _match_book_orders(book_orders)
             _print_deals(deals)
 
-            await asyncio.sleep(0.5)
+            await asyncio.sleep(1)
     except KeyboardInterrupt:
         pass
     finally:
