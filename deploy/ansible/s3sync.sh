c82b6bf7e58ca09a9439283076deb9a73975d528
Filipe C Menezes <fcmenezes87@gmail.com>
2024-07-09 10:45:26 +0100

    chore: keep s3 sync in ansible

--- deploy/ansible/s3sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-current_dir=$(dirname "$0")
-
-aws s3 sync "$current_dir/../../data" "s3://$S3BUCKET" --exclude .gitkeep
-ls -la "$current_dir/../../data" | grep -v "$(date +%Y-%m-%d).csv" | grep -v .gitkeep | xargs rm -rf
