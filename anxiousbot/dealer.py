594f31adcb3e5ac66ed5f7fb8ad1eee178fdf200
Filipe C Menezes <fcmenezes87@gmail.com>
2024-08-04 05:50:12 +0100

    chore: add redis close

--- anxiousbot/dealer.py
+++ anxiousbot/dealer.py
@@ -258,6 +258,7 @@ class Dealer:
 
     async def close(self):
         tasks = [
+            self._redis_handler.close(),
             self._exchange_handler.close(),
             self._bot_handler.close(),
             self._order_book_handler.close(),
