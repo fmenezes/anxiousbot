78b869bfdaef9a47cb3df71dee89b7243b80e32e
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2025-01-21 06:06:05 +0000

    minify in prod

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import  dotenv from "dotenv";
 
 dotenv.config();
 
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
 export default defineConfig({
   define: {
     ENDPOINT_URL: `"${process.env.ENDPOINT_URL}"`,
@@ -10,7 +12,7 @@ export default defineConfig({
   base: '/anxiousbot/',
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    minify: false
+    sourcemap: process.env.NODE_ENV === 'development',
+    minify: process.env.NODE_ENV !== 'development'
   },
 });
